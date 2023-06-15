cd3c4402d094f7456cb11a84ee2790e4b34abb02
Nicole Murff <mur20031@byui.edu>
2023-06-15 00:12:29 -0500

    updated routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,5 +3,8 @@ const contactsController = require('../controllers/contacts');
 
 routes.get('/', contactsController.getAllData);
 routes.get('/:id', contactsController.getSingleData);
+routes.get('/:id', contactsController.createContact);
+routes.get('/:id', contactsController.updateContact);
+routes.get('/:id', contactsController.deleteContact);
 
 module.exports = routes;
